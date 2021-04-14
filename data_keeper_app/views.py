2f08916e6d6983de547dc6989c82f70220a34e88
yahel <yahel.fri@gmail.com>
2021-04-14 13:55:20 +0300

    phone_number bug fixed

--- data_keeper_app/views.py
+++ data_keeper_app/views.py
@@ -1,11 +1,8 @@
 import json
-from django_ajax.decorators import ajax
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render
-#from .forms import UserDetailsForm
 from .forms import app_user
 from .models import UserDetails
-from .serializers import UserDetailsSerializer
 from django.forms.models import  model_to_dict
 
 
@@ -15,7 +12,6 @@ def welcome_page(request):
 
 
 def test(request):
-    printshit("param")
     return HttpResponse(
         json.dumps('successful'),
         content_type="application/json"
@@ -23,9 +19,6 @@ def test(request):
 
 
 def home_page(request):
-    if request.method == 'POST':
-        print(request.session['paz'])
-        form = app_user(request.POST or None)
     return render(request, 'home_page.html')
 
 
@@ -43,21 +36,14 @@ def input_click_evnt(request):
 
 
 def user_details_view(request):
-    if UserDetails.objects.all().count()>0: ## restore data
+    if UserDetails.objects.all().count()>0:   # restore data
         user_details = UserDetails.objects.all().first()
         form = app_user(model_to_dict(user_details))
-        printshit(form)
         return render(request, 'Log-in.html', {'form': form})
-    else: # new session
+    else:  # new session
         if request.method == 'POST':
-            printshit(request.body)
             form = app_user(request.POST or None)
             return render(request, 'Log-in.html', {'form':form})
 
-
-def printshit(value):
-    print('\n\n***********************************\n\n')
-    print(value)
-    print('\n\n***********************************\n\n')
 
 
