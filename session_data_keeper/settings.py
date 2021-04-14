0f0d4a7e8cb9dffde789605196d08e1ca49a613f
yahel <yahel.fri@gmail.com>
2021-04-14 13:33:48 +0300

    initial commit5

--- session_data_keeper/settings.py
+++ session_data_keeper/settings.py
@@ -152,4 +152,4 @@ STATICFILES_DIRS = (
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
