eb7d5654dd947bcef6c4af84bc260a50f7212a37
yahel <yahel.fri@gmail.com>
2021-04-14 13:28:36 +0300

    initial commi4

--- session_data_keeper/settings.py
+++ session_data_keeper/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,3 +151,5 @@ STATICFILES_DIRS = (
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
